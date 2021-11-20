8933310c1cc1e2a7af4ec45904a7efcd99e12381
synesteesia <mikko.ahokas@hotmail.com>
2021-11-20 10:51:34 +0200

    viikko4tehtava5.9.

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -50,4 +50,14 @@ class TestOstoskori(unittest.TestCase):
     def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_yksi_ostosolio(self):
         maito = Tuote("Maito", 3)
         self.kori.lisaa_tuote(maito)
-        self.assertEqual(self.kori.ostokset(), 1)
+        self.assertEqual(len(self.kori.ostokset()), 1)
+
+    def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_yksi_ostosolio_jolla_oikea_tuotteen_nimi_ja_maara(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+ 
+        ostos = self.kori.ostokset()[0]
+        self.assertEqual(len(self.kori.ostokset()), 1)
+        self.assertEqual(ostos.tuotteen_nimi(), "Maito")
+
+    
