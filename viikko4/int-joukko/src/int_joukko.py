5ec3de548715a843aa2e06938c123a510a32e47c
synesteesia <mikko.ahokas@hotmail.com>
2021-11-21 10:38:46 +0200

    viikko4tehtava6

--- viikko4/int-joukko/src/int_joukko.py
+++ viikko4/int-joukko/src/int_joukko.py
@@ -7,144 +7,117 @@ class IntJoukko:
         if kapasiteetti is None:
             self.kapasiteetti = KAPASITEETTI
         elif not isinstance(kapasiteetti, int) or kapasiteetti < 0:
-            raise Exception("Väärä kapasiteetti")  # heitin vaan jotain :D
+            raise Exception("Kapasiteetin tulee olla nollaa suuremi kokonaisluku")  
         else:
             self.kapasiteetti = kapasiteetti
 
         if kasvatuskoko is None:
             self.kasvatuskoko = OLETUSKASVATUS
-        elif not isinstance(kapasiteetti, int) or kapasiteetti < 0:
-            raise Exception("kapasiteetti2")  # heitin vaan jotain :D
+        elif not isinstance(kasvatuskoko, int) or kasvatuskoko < 0:
+            raise Exception("Kasvatuskoon tulee olla nollaa suuremi kokonaisluku") 
         else:
             self.kasvatuskoko = kasvatuskoko
 
-        self.ljono = [0] * self.kapasiteetti
-
+        self.joukko = [0] * self.kapasiteetti
         self.alkioiden_lkm = 0
 
-    def kuuluu(self, n):
-        on = 0
 
-        for i in range(0, self.alkioiden_lkm):
-            if n == self.ljono[i]:
-                on = on + 1
-
-        if on > 0:
-            return True
-        else:
-            return False
-
-    def lisaa(self, n):
-        ei_ole = 0
-
-        if self.alkioiden_lkm == 0:
-            self.ljono[0] = n
-            self.alkioiden_lkm = self.alkioiden_lkm + 1
-            return True
-        else:
-            pass
-
-        if not self.kuuluu(n):
-            self.ljono[self.alkioiden_lkm] = n
-            self.alkioiden_lkm = self.alkioiden_lkm + 1
-
-            if self.alkioiden_lkm % len(self.ljono) == 0:
-                taulukko_old = self.ljono
-                self.kopioi_taulukko(self.ljono, taulukko_old)
-                self.ljono = [0] * (self.alkioiden_lkm + self.kasvatuskoko)
-                self.kopioi_taulukko(taulukko_old, self.ljono)
-
-            return True
-
+    def kuuluuko_joukkoon(self, alkio):
+        for i in range(self.alkioiden_lkm):
+            if alkio == self.joukko[i]:
+                return True
         return False
 
-    def poista(self, n):
-        kohta = -1
-        apu = 0
 
-        for i in range(0, self.alkioiden_lkm):
-            if n == self.ljono[i]:
-                kohta = i  # siis luku löytyy tuosta kohdasta :D
-                self.ljono[kohta] = 0
-                break
+    def kasvata_kapasiteettia(self):
+        self.kasvatuskoko *= 2
+        uusi_joukko = [0] * (self.kasvatuskoko)
+        for i in range(len(self.joukko)):
+            uusi_joukko[i] = self.joukko[i]
+        self.joukko = uusi_joukko
 
-        if kohta != -1:
-            for j in range(kohta, self.alkioiden_lkm - 1):
-                apu = self.ljono[j]
-                self.ljono[j] = self.ljono[j + 1]
-                self.ljono[j + 1] = apu
 
-            self.alkioiden_lkm = self.alkioiden_lkm - 1
+    def lisaa_alkio(self, alkio):
+        if not self.kuuluuko_joukkoon(alkio):
+            self.joukko[self.alkioiden_lkm] = alkio
+            self.alkioiden_lkm += 1
+
+            if self.alkioiden_lkm == len(self.joukko):
+                self.kasvata_kapasiteettia()
             return True
-
         return False
 
-    def kopioi_taulukko(self, a, b):
-        for i in range(0, len(a)):
-            b[i] = a[i]
+    def poista_alkio(self, alkio):
+        for i in range(self.alkioiden_lkm):
+            if alkio == self.joukko[i]:
+                for j in range(i, self.alkioiden_lkm - 1):
+                   apu = self.joukko[j]
+                   self.joukko[j] = self.joukko[j + 1]
+                   self.joukko[j + 1] = apu
+                self.alkioiden_lkm -= 1
+                return True
+        return False
 
-    def mahtavuus(self):
+    def alkioiden_maara(self):
         return self.alkioiden_lkm
 
     def to_int_list(self):
         taulu = [0] * self.alkioiden_lkm
 
-        for i in range(0, len(taulu)):
-            taulu[i] = self.ljono[i]
+        for i in range(len(taulu)):
+            taulu[i] = self.joukko[i]
 
         return taulu
 
     @staticmethod
     def yhdiste(a, b):
-        x = IntJoukko()
-        a_taulu = a.to_int_list()
-        b_taulu = b.to_int_list()
+        yhdiste = IntJoukko()
+        a_joukko = a.to_int_list()
+        b_joukko = b.to_int_list()
 
-        for i in range(0, len(a_taulu)):
-            x.lisaa(a_taulu[i])
+        for i in range(len(a_joukko)):
+            yhdiste.lisaa_alkio(a_joukko[i])
 
-        for i in range(0, len(b_taulu)):
-            x.lisaa(b_taulu[i])
+        for i in range(len(b_joukko)):
+            yhdiste.lisaa_alkio(b_joukko[i])
 
-        return x
+        return yhdiste
 
     @staticmethod
     def leikkaus(a, b):
-        y = IntJoukko()
-        a_taulu = a.to_int_list()
-        b_taulu = b.to_int_list()
+        leikkaus = IntJoukko()
+        a_joukko = a.to_int_list()
+        b_joukko = b.to_int_list()
 
-        for i in range(0, len(a_taulu)):
-            for j in range(0, len(b_taulu)):
-                if a_taulu[i] == b_taulu[j]:
-                    y.lisaa(b_taulu[j])
+        for i in range(len(a_joukko)):
+            for j in range(0, len(b_joukko)):
+                if a_joukko[i] == b_joukko[j]:
+                    leikkaus.lisaa_alkio(b_joukko[j])
 
-        return y
+        return leikkaus
 
     @staticmethod
     def erotus(a, b):
-        z = IntJoukko()
-        a_taulu = a.to_int_list()
-        b_taulu = b.to_int_list()
+        erotus = IntJoukko()
+        a_joukko = a.to_int_list()
+        b_joukko = b.to_int_list()
 
-        for i in range(0, len(a_taulu)):
-            z.lisaa(a_taulu[i])
+        for i in range(len(a_joukko)):
+            erotus.lisaa_alkio(a_joukko[i])
 
-        for i in range(0, len(b_taulu)):
-            z.poista(b_taulu[i])
+        for i in range(len(b_joukko)):
+            erotus.poista_alkio(b_joukko[i])
 
-        return z
+        return erotus
 
     def __str__(self):
         if self.alkioiden_lkm == 0:
             return "{}"
-        elif self.alkioiden_lkm == 1:
-            return "{" + str(self.ljono[0]) + "}"
         else:
             tuotos = "{"
             for i in range(0, self.alkioiden_lkm - 1):
-                tuotos = tuotos + str(self.ljono[i])
+                tuotos = tuotos + str(self.joukko[i])
                 tuotos = tuotos + ", "
-            tuotos = tuotos + str(self.ljono[self.alkioiden_lkm - 1])
+            tuotos = tuotos + str(self.joukko[self.alkioiden_lkm - 1])
             tuotos = tuotos + "}"
             return tuotos
--- viikko4/int-joukko/src/tests/int_joukko_test.py
+++ viikko4/int-joukko/src/tests/int_joukko_test.py
@@ -5,14 +5,14 @@ from int_joukko import IntJoukko
 class TestIntJoukko(unittest.TestCase):
     def setUp(self):
         self.joukko = IntJoukko()
-        self.joukko.lisaa(10)
-        self.joukko.lisaa(3)
+        self.joukko.lisaa_alkio(10)
+        self.joukko.lisaa_alkio(3)
 
     def tee_joukko(self, *luvut):
         joukko = IntJoukko()
 
         for luku in luvut:
-            joukko.lisaa(luku)
+            joukko.lisaa_alkio(luku)
 
         return joukko
 
@@ -20,39 +20,39 @@ class TestIntJoukko(unittest.TestCase):
         lisattavat = [1, 2, 4, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
 
         for luku in lisattavat:
-            joukko.lisaa(luku)
+            joukko.lisaa_alkio(luku)
 
-        self.assertEqual(joukko.mahtavuus(), 14)
-        self.assertTrue(joukko.kuuluu(11))
-        joukko.poista(11)
-        self.assertFalse(joukko.kuuluu(11))
-        self.assertEqual(joukko.mahtavuus(), 13)
+        self.assertEqual(joukko.alkioiden_maara(), 14)
+        self.assertTrue(joukko.kuuluuko_joukkoon(11))
+        joukko.poista_alkio(11)
+        self.assertFalse(joukko.kuuluuko_joukkoon(11))
+        self.assertEqual(joukko.alkioiden_maara(), 13)
 
     def test_lukuja_lisatty_maara(self):
-        self.joukko.lisaa(4)
-        self.assertEqual(self.joukko.mahtavuus(), 3)
+        self.joukko.lisaa_alkio(4)
+        self.assertEqual(self.joukko.alkioiden_maara(), 3)
 
     def test_sama_luku_menee_joukkoon_vaan_kerran(self):
-        self.joukko.lisaa(10)
-        self.joukko.lisaa(3)
-        self.assertEqual(self.joukko.mahtavuus(), 2)
+        self.joukko.lisaa_alkio(10)
+        self.joukko.lisaa_alkio(3)
+        self.assertEqual(self.joukko.alkioiden_maara(), 2)
 
     def test_vain_lisatyt_luvut_loytyvat(self):
-        self.assertTrue(self.joukko.kuuluu(10))
-        self.assertFalse(self.joukko.kuuluu(5))
-        self.assertTrue(self.joukko.kuuluu(3))
+        self.assertTrue(self.joukko.kuuluuko_joukkoon(10))
+        self.assertFalse(self.joukko.kuuluuko_joukkoon(5))
+        self.assertTrue(self.joukko.kuuluuko_joukkoon(3))
 
     def test_poistettu_ei_ole_enaa_joukossa(self):
-        self.joukko.poista(3)
-        self.assertFalse(self.joukko.kuuluu(3))
-        self.assertEqual(self.joukko.mahtavuus(), 1)
+        self.joukko.poista_alkio(3)
+        self.assertFalse(self.joukko.kuuluuko_joukkoon(3))
+        self.assertEqual(self.joukko.alkioiden_maara(), 1)
 
     def test_palautetaan_oikea_taulukko(self):
         odotettu = [3, 55, 99]
 
-        self.joukko.lisaa(55)
-        self.joukko.poista(10)
-        self.joukko.lisaa(99)
+        self.joukko.lisaa_alkio(55)
+        self.joukko.poista_alkio(10)
+        self.joukko.lisaa_alkio(99)
 
         vastaus = self.joukko.to_int_list()
 
@@ -72,7 +72,7 @@ class TestIntJoukko(unittest.TestCase):
 
     def test_merkkijonoesitys_toimii_yhden_kokeisella_joukolla(self):
         joukko = IntJoukko()
-        joukko.lisaa(1)
+        joukko.lisaa_alkio(1)
         self.assertEqual(str(joukko), "{1}")
 
     def test_merkkijonoesitys_toimii_tyhjalla_joukolla(self):
