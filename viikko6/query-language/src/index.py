1e24eaba6eb4b2b03136ee0546b49ae326e0c796
synesteesia <mikko.ahokas@hotmail.com>
2021-12-02 16:39:55 +0200

    viikko6.3

---
+++ viikko6/query-language/src/index.py
@@ -0,0 +1,23 @@
+from statistics import Statistics
+from player_reader import PlayerReader
+from matchers import And,Or, HasAtLeast, PlaysIn, Not
+
+def main():
+    url = "https://nhlstatisticsforohtu.herokuapp.com/players.txt"
+    reader = PlayerReader(url)
+    stats = Statistics(reader)
+
+    matcher = Or(
+       HasAtLeast(30, "goals"),
+       HasAtLeast(50, "assists")
+    )
+
+
+
+
+    for player in stats.matches(matcher):
+        print(player)
+
+
+if __name__ == "__main__":
+    main()
---
+++ viikko6/query-language/src/matchers.py
@@ -0,0 +1,74 @@
+class And:
+    def __init__(self, *matchers):
+        self._matchers = matchers
+    
+    def matches(self, player):
+        for matcher in self._matchers:
+            if not matcher.matches(player):
+                return False
+        
+        return True
+
+
+class Or:
+    def __init__(self, *matchers):
+        self._matchers = matchers
+    
+    def matches(self, player):
+        for matcher in self._matchers:
+            if matcher.matches(player):
+                return True
+        
+        return False
+
+
+class PlaysIn:
+    def __init__(self, team):
+        self._team = team
+
+    def matches(self, player):
+        return player.team == self._team
+
+
+class HasAtLeast:
+    def __init__(self, value, attr):
+        self._value = value
+        self._attr = attr
+
+    def matches(self, player):
+        player_value = getattr(player, self._attr)
+
+        return player_value >= self._value
+
+
+class HasFewerThan:
+    def __init__(self, value, attr):
+        self._value = value
+        self._attr = attr
+
+    def matches(self, player):
+        player_value = getattr(player, self._attr)
+
+        return player_value < self._value
+
+
+class Not:
+    def __init__(self, condition):
+        self._condition = condition
+
+    def matches(self, player):
+        return not self._condition.matches(player)
+
+
+class All:
+    def __init__(self, condition):
+        self._condition = condition
+
+    def matches(self, *players):
+        for player in players:
+            if not self._condition.matches(player):
+                return False
+        
+        return True
+
+
